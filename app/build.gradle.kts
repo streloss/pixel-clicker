edb67ac80f5a255974ff052fb242e76eea89fa06
streloss <fufypdtx2@gmail.com>
2026-09-10 17:16:31 +0900

    feat: Add Pixel Clicker Android 16 app with GitHub Actions CI

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -20,9 +20,19 @@ android {
         }
     }
 
+    signingConfigs {
+        create("release") {
+            storeFile = file("release.jks")
+            storePassword = "pixelclicker123"
+            keyAlias = "pixelkey"
+            keyPassword = "pixelclicker123"
+        }
+    }
+
     buildTypes {
         release {
             isMinifyEnabled = false
+            signingConfig = signingConfigs.getByName("release")
             proguardFiles(
                 getDefaultProguardFile("proguard-android-optimize.txt"),
                 "proguard-rules.pro"
